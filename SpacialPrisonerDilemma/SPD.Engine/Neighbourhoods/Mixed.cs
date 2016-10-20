9b1c239123fee29fd48e65ce773e5e74d9d8e44c
Błażej <Baej@BLAISECOMP>
2016-10-20 16:24:53 +0200

    Dodane mieszane sąsiedztwo

---
+++ SpacialPrisonerDilemma/SPD.Engine/Neighbourhoods/Mixed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpacialPrisonerDilemma.Engine;
+using SpacialPrisonerDilemma.Engine.Neighbourhoods;
+
+namespace SPD.Engine.Neighbourhoods
+{
+    class Mixed:INeighbourhood
+    {
+        private INeighbourhood[] n;
+        public Mixed(params INeighbourhood[] neighbourhoods)
+        {
+            n = neighbourhoods;
+        }
+
+        public IEnumerable<Coord> GetNeighbours(int x, int y)
+        {
+            return n.SelectMany(z => z.GetNeighbours(x, y));
+        }
+
+        public IEnumerable<Coord> GetNeighbours(Coord c)
+        {
+            return GetNeighbours(c.X, c.Y);
+        }
+
+        public IEnumerable<Coord> GetHalfNeighbours(int x, int y)
+        {
+            return n.SelectMany(z => z.GetHalfNeighbours(x, y));
+        }
+
+        public IEnumerable<Coord> GetHalfNeighbours(Coord c)
+        {
+            return GetHalfNeighbours(c.X, c.Y);
+        }
+    }
+}
